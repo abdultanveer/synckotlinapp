b91324145a5d7815839ac1c081758346fd340103
abdul <abdul.tanveer@gmail.com>
2021-12-16 16:34:51 +0530

    options menu.. when expression

--- app/src/main/java/com/abdul/synckotlinapp/MainActivity.kt
+++ app/src/main/java/com/abdul/synckotlinapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.abdul.synckotlinapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.Toast
 
@@ -12,6 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         emailEt = findViewById(R.id.etEmail)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        super.onOptionsItemSelected(item)
+        when(item.itemId){
+            R.id.miSettings -> { Toast.makeText(this,"settings",Toast.LENGTH_SHORT).show()}
+            R.id.miLogout -> {Toast.makeText(this,"logout",Toast.LENGTH_SHORT).show()}
+        }
+
+        return true
     }
 
     fun handleClick(view: android.view.View) {
