3c2eb91c3cfd0ba3587a938e526b196e5253582d
abdul <abdul.tanveer@gmail.com>
2021-12-21 15:07:39 +0530

    whowroteit-2 get the json string

--- app/src/main/java/com/abdul/synckotlinapp/whowroteit/FetchBookTask.kt
+++ app/src/main/java/com/abdul/synckotlinapp/whowroteit/FetchBookTask.kt
@@ -8,10 +8,16 @@ class FetchBookTask(mTitleText: TextView, mAuthorText: TextView)
                                 : AsyncTask<String,Void,String>() {
     var TAG =FetchBookTask::class.java.simpleName
 
+    //https://www.googleapis.com/books/v1/volumes?q=othello&maxResults=10&printType=books
     override fun doInBackground(vararg bookName: String?): String {
         Log.i(TAG,"search for the book named ---"+bookName[0].toString())
         //it has to return the json string
-        return ""
+        return NetworkUtils.getBookInfo(bookName[0].toString())
+    }
+
+    override fun onPostExecute(jsonString: String?) {
+        super.onPostExecute(jsonString)
+        Log.i(TAG,"result is \n"+jsonString)
     }
 
 }
---
+++ app/src/main/java/com/abdul/synckotlinapp/whowroteit/NetworkUtils.java
@@ -0,0 +1,78 @@
+package com.abdul.synckotlinapp.whowroteit;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetworkUtils {
+
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String BOOK_BASE_URL = "https://www.googleapis.com/books/v1/volumes?"; // Base URI for the    Books API
+    private static final String QUERY_PARAM = "q"; // Parameter for the search string ie bookname
+    private static final String MAX_RESULTS = "maxResults"; // Parameter that limits search results
+    private static final String PRINT_TYPE = "printType"; // Parameter to filter by print type
+
+
+    public static String getBookInfo(String bookName) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String bookJSONString = null;
+        try {
+            Uri builtURI = Uri.parse(BOOK_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, bookName)
+                    .appendQueryParameter(MAX_RESULTS, "3")
+                    .appendQueryParameter(PRINT_TYPE, "books")
+                    .build();
+            URL requestURL = new URL(builtURI.toString());
+            Log.i(LOG_TAG, requestURL.toString());
+
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+            urlConnection.setRequestMethod("GET");//GET -- read db, POST -- write db, PUT -- update db, DELETE
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                /* Since it's JSON, adding a newline isn't necessary (it won't affect         parsing) but it does make debugging a *lot* easier if you print out the            completed buffer for debugging. */
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {// Stream was empty. No point in parsing.
+                return null;
+            }
+            bookJSONString = buffer.toString();
+            Log.i(LOG_TAG, requestURL.toString());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Log.d(LOG_TAG, bookJSONString);
+            return bookJSONString;
+        }
+    }
+
+} 
