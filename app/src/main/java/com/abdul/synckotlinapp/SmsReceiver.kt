27635b7c75fd49f4ebe338b1f469ee358f6f74ed
abdul <abdul.tanveer@gmail.com>
2021-12-22 12:27:09 +0530

    smsbroadcast - extract the message

--- app/src/main/java/com/abdul/synckotlinapp/SmsReceiver.kt
+++ app/src/main/java/com/abdul/synckotlinapp/SmsReceiver.kt
@@ -3,12 +3,57 @@ package com.abdul.synckotlinapp
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.telephony.SmsMessage
 import android.widget.Toast
+import android.os.Build
 
+
+
+
+/**
+ * complete the task within 5 secs
+ */
 class SmsReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        Toast.makeText(context,"otp received",Toast.LENGTH_SHORT).show()
+        showSmsBody(intent,context);
+    }
+
+    private fun showSmsBody(intent: Intent, context: Context) {
+        var  bundle: Bundle
+        var  currentSMS: SmsMessage
+        var  message: String
+        if (intent.action.equals("android.provider.Telephony.SMS_RECEIVED")) {
+            bundle = intent.extras!!
+            if (bundle != null) {
+                val pdu_Objects = bundle.get("pdus") as Array<Any>
+                if (pdu_Objects != null) {
+                    for (aObject in pdu_Objects) {
+                        currentSMS = getIncomingMessage(aObject, bundle)
+                        val senderNo: String = currentSMS.getDisplayOriginatingAddress()
+                        message = currentSMS.getDisplayMessageBody()
+                        Toast.makeText(context,
+                            "senderNum: $senderNo :\n message: $message",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                   // abortBroadcast()
+                    // End of loop
+                }
+            }
+        }
+    }
+
+    private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage {
+        val currentSMS: SmsMessage
+        currentSMS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val format = bundle.getString("format")
+            SmsMessage.createFromPdu(aObject as ByteArray, format)
+        } else {
+            SmsMessage.createFromPdu(aObject as ByteArray)
+        }
+        return currentSMS
     }
 }
