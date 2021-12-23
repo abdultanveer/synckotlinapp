da7d85cdb88a57cfe3f84e7813592c810f6cc303
abdul <abdul.tanveer@gmail.com>
2021-12-23 11:42:44 +0530

    bound services -2

--- app/src/main/java/com/abdul/synckotlinapp/MyService.kt
+++ app/src/main/java/com/abdul/synckotlinapp/MyService.kt
@@ -3,22 +3,31 @@ package com.abdul.synckotlinapp
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 import android.widget.Toast
+import java.util.*
 
 class MyService : Service() {
     var TAG = MyService::class.java.simpleName
 
+    private val mGenerator = Random()
+
+    val randomNumber: Int
+    get() = mGenerator.nextInt(100)
+
+
+
     override fun onCreate() {
         super.onCreate()
-        Log.i(TAG,"oncreate-myservice")
+        Log.i(TAG, "oncreate-myservice")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.i(TAG,"onstartcommand-myservice")
+        Log.i(TAG, "onstartcommand-myservice")
         var songname = intent?.getStringExtra("songname")
-        Log.i(TAG,"i am playing--"+songname)
+        Log.i(TAG, "i am playing--" + songname)
         /*var  mplayer = MediaPlayer.create(this,R.raw.music)
         mplayer.start()*/
 
@@ -28,11 +37,19 @@ class MyService : Service() {
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i(TAG,"ondestroy-myservice")
+        Log.i(TAG, "ondestroy-myservice")
 
     }
 
+    private val binder = LocalBinder() //binder is like a pipe between service and the activity
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return binder
+    }
+
+    inner class LocalBinder : Binder() {
+
+        fun getService(): MyService = this@MyService
+
     }
 }
